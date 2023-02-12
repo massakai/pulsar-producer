31f6787a1fb472868ddd2a7b34219216455040c0
Masashi SAKAI <masashi.sakai1986@gmail.com>
2023-02-13 00:13:38 +0900

    Spring Initializrが生成した雛形を追加

---
+++ src/main/java/com/github/massakai/pulsar/producer/PulsarProducerApplication.java
@@ -0,0 +1,13 @@
+package com.github.massakai.pulsar.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsarProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PulsarProducerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/massakai/pulsar/producer/PulsarProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.massakai.pulsar.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsarProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
